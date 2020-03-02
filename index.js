c3b300c091a6315be6b28a5b52692317742e1156
Ben Flanders <benjamin.flanders96@gmail.com>
2020-03-02 11:59:30 -0500

    app push

---
+++ index.js
@@ -0,0 +1,22 @@
+function openform(){
+    document.getElementById("my-form").style.display="block";
+}
+
+function closeform(){
+    document.getElementById("my-form").style.display="none";
+}
+
+var acc = document.getElementsByClassName("accordion");
+		var i;
+
+		for (i = 0; i < acc.length; i++) {
+		  acc[i].addEventListener("click", function() {
+		    this.classList.toggle("active");
+		    var panel = this.nextElementSibling;
+		    if (panel.style.maxHeight){
+		      panel.style.maxHeight = null;
+		    } else {
+		      panel.style.maxHeight = panel.scrollHeight + "px";
+		    } 
+		  });
+		}
